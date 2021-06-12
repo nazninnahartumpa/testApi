90f532e6375254280d3b075fbd83952b1ab76956
naznin <naznin.nahar@accelx.net>
2021-06-12 18:14:35 +0600

    testApi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'apiprj';
+
+  constructor(private http:HttpClient){}
+
+  onSubmit(data){
+    this.http.post("https://bnopenmrs.accelx.net/openmrs/ws/rest/v1/person", data = {
+      "names": [
+          {
+          "givenName": "New Name",
+          "familyName": "New name"
+          }
+      ],
+      "gender": "M",
+      "birthdate": "1997-09-02",
+      "addresses": [
+          {
+          "address1": "30, Vivekananda Layout, Munnekolal,Marathahalli",
+          "cityVillage": "Bengaluru",
+          "country": "India",
+          "postalCode": "560037"
+          }
+      ]
+  }).subscribe((result)=>{
+    console.log("result", result);
+  })
+    console.warn(data);
+  }
+  
+
 }
